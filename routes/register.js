ddedb18f212d049cb6bd2097bc1c85e48825f9fa
harmakit <harmakit@gmail.com>
2018-03-07 20:40:39 +0700

    some formatting fixes

--- routes/register.js
+++ routes/register.js
@@ -4,22 +4,22 @@ const joi = require('joi');
 
 
 const schema = joi.object().keys({
-    name: joi.string().alphanum().min(3).max(30),
-    password: joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
-    repassword: joi.string().required().valid(joi.ref('password')),
-    email: joi.string().email()
+  name: joi.string().alphanum().min(3).max(30),
+  password: joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
+  repassword: joi.string().required().valid(joi.ref('password')),
+  email: joi.string().email()
 });
 
 router.post('/', function (req, res) {
   var valid = joi.validate({
-        name: req.body.name,
-        password: req.body.password,
-        repassword: req.body.repassword,
-        email: req.body.email
-      }, schema);
+    name: req.body.name,
+    password: req.body.password,
+    repassword: req.body.repassword,
+    email: req.body.email
+  }, schema);
 
-  res.render('register', {
-    error: valid.error} )
+res.render('register', {
+  error: valid.error} )
 });
 
 router.get('/', function(req, res) {
