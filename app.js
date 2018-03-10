583320a8dc031540207b2db4987a952930dc7719
harmakit <harmakit@gmail.com>
2018-03-10 20:21:59 +0700

    setting up branch

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); 
 app.use(cookieParser());
 app.use(flash());
 
